0681972872ccd78a58562a085ae7e3efb7652a7b
WebMad Developer4 <webmad.developer4@gmail.com>
2021-07-13 09:36:52 +0530

    home page spacing and allignment has been done footer work is remainiong

---
+++ GoodBrick/GoodBrick/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoodBrick.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index() => View();
+        public ActionResult AboutUs() => View();
+        public ActionResult ContactUs() => View();
+        public ActionResult Gallery() => View();
+        public ActionResult What_are_stabilizedmud_bricks() => View();
+        public ActionResult Products() => View();
+        public ActionResult Careers() => View();
+        public ActionResult Consultation() => View();
+    }
+}
---
+++ GoodBrick/GoodBrick/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoodBrick.MvcApplication" Language="C#" %>
